bd4af24af7004ec8d90dcff695392c642c36df78
kostromin59 <ngskostromin@gmail.com>
2025-11-26 19:11:48 +0500

    feat: media migrations

---
+++ migrations/20251126140813_add_media.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS media (
+  id UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+  filetype TEXT NOT NULL,
+  uri TEXT NOT NULL,
+  post_id UUID,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  FOREIGN KEY(post_id) REFERENCES posts(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
